2ed55fb062e7f27be8da9b8c25973ae76bf6a0e5
Leonard <leonard.faix@googlemail.com>
2021-04-15 17:44:38 +0200

    working withou boat

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ class Point:
         self.y = y
     def __lt__(self, other):
         return self.x + self.y < other.x + other.y
+    def __eq__(self, other):
+        return self.x == other.x and self.y == other.y
     def __str__(self):
         return "("+str(self.x)+","+str(self.y)+")"
     def __hash__(self):
@@ -31,16 +33,17 @@ def read_csv():
 
 
 board = read_csv()
-for x in board:
-    for y in x:
-        print(y, end=" ")
+for y in board:
+    for x in y:
+        print(x, end=" ")
     print()
 
-
 #Geschwindigkeiten
-v_weg = 9
-v_wiese_boot = 6
-v_berg = 4
+v_weg = 2
+v_wiese_boot = 4
+v_wald = 6
+v_berg = 9
+v_nicht_begehbar = 10000
 
 # fluss ohne boot nicht überquerbar
 # Einweg boot
@@ -50,18 +53,30 @@ def dist(a: Point, b: Point):
     return abs(a.x-b.x) + abs(a.y-b.y)
 
 def kosten(a: Point):
-    feld = board[a.x][a.y]
-    return 1
+    feld = board[a.y][a.x]
+    if feld == 0: # Wasser
+        return v_nicht_begehbar
+    elif feld == 1: # Wiese
+        return v_wiese_boot
+    elif feld == 2: # Weg
+        return v_weg
+    elif feld == 3: # bergiges Gelände
+        return v_berg
+    elif feld == 4: # Wald
+        return v_wald
+    else:
+        print("ERROR")
+        return -1
 
 def nachbar(i: Point):
     neighbors = [Point(i.x+1,i.y),Point(i.x,i.y+1),Point(i.x-1,i.y),Point(i.x,i.y-1)]
-    moin = [n for n in neighbors if n.x >= 0 and n.y >= 0 and n.x < len(board) and n.y < len(board[0])]
+    moin = [n for n in neighbors if n.x >= 0 and n.y >= 0 and n.x < len(board[0]) and n.y < len(board)]
     return moin
 
 
 def a_star(board, start: Point, goal: Point):
     frontier = PriorityQueue()
-    frontier.put(start, 0)
+    frontier.put((0, start))
     came_from = dict()
     cost_so_far = dict()
     came_from[start] = None
@@ -69,25 +84,40 @@ def a_star(board, start: Point, goal: Point):
     
     while not frontier.empty():
         current = frontier.get()
-        print(current)
-        if current == goal:
+        currentPoint = current[1]
+        print("current",current[0],current[1], "kosten", kosten(currentPoint))
+        if currentPoint == goal:
             break
         
-        for next in nachbar(current):
-            new_cost = cost_so_far[current] + kosten(next)
-            print(next, new_cost)
-            if next not in cost_so_far or new_cost < cost_so_far[next]:
-                cost_so_far[next] = new_cost
-                priority = new_cost + dist(next, goal)
-                frontier.put(next, priority)
-                came_from[next] = current
+        for p in nachbar(currentPoint):
+            new_cost = cost_so_far[currentPoint] + kosten(p)
+            print(p, new_cost)
+            if p not in cost_so_far or new_cost < cost_so_far[p]:
+                cost_so_far[p] = new_cost
+                priority = new_cost + dist(p, goal)
+                frontier.put((priority, p))
+                came_from[p] = currentPoint
 
     return came_from, cost_so_far
 
-came_from, cost_so_far = a_star(board, Point(7,7), Point(4,5))
-print(came_from, cost_so_far)
+
+startTile = Point(0,0)
+goalTile = Point(39,40)
+came_from, cost_so_far = a_star(board, startTile, goalTile)
+
+print("fjkdlsa", kosten(Point(28,33)))
+
+path = []
+toTile = goalTile
+path.append(toTile)
+while toTile != startTile:
+    fromTile = came_from[toTile]
+    path.append(fromTile)
+    toTile = fromTile
+
+for p in path:
+    print(p)
 
 vizu = Vizu(board)
-path = [(1,5),(2,5),(2,6),(2,7),(1,7)]
 vizu.draw_path(path)
 vizu.run()
--- vizualisation.py
+++ vizualisation.py
@@ -4,6 +4,7 @@ from pyglet import shapes
 
 class Vizu(pyglet.window.Window):
     def __init__(self, board):
+        super().__init__(width=1000, height=1000)
         self.batch = pyglet.graphics.Batch()
         self.b = []
 
@@ -14,10 +15,9 @@ class Vizu(pyglet.window.Window):
         for row in board:
             for cell in row:
                 number = int(cell)
-                self.b.append(shapes.Rectangle((i%40)*rect_width, int(i/40)*rect_width, rect_width, rect_width, color=self.get_color(number), batch=self.batch))
+                self.b.append(shapes.Rectangle((i%40)*rect_width, self.height - int(1+i/40)*rect_width, rect_width, rect_width, color=self.get_color(number), batch=self.batch))
                 i+=1
 
-        super().__init__(width=1000, height=1000)
 
     def on_draw(self):
         self.clear()
@@ -28,13 +28,13 @@ class Vizu(pyglet.window.Window):
         if i == 0:
             return (10,30,200)
         elif i == 1:
-            return (10,200,30)
+            return (60,200,90)
         elif i == 2:
             return (200,150,150)
         elif i == 3:
             return (200,200,200)
         elif i == 4:
-            return (10,200,20)
+            return (0,100,0)
         else:
             return (0,0,0)
     
@@ -47,8 +47,8 @@ class Vizu(pyglet.window.Window):
         for i in range(len(points)-1):
             start = points[i]
             end = points[i+1]
-            self.path.append(shapes.Line((start[0]+.5)*rect_width, (start[1]+.5)*rect_width, 
-                             (end[0]+.5)*rect_width, (end[1]+.5)*rect_width, width=5, color=(200,50,50), batch=self.batch))
+            self.path.append(shapes.Line((start.x+.5)*rect_width, self.height-(start.y+.5)*rect_width, 
+                             (end.x+.5)*rect_width, self.height-(end.y+.5)*rect_width, width=5, color=(200,50,50), batch=self.batch))
 
 
 
